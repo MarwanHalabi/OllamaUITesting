c0d450f144b8c77158a31a83930a398a5934cf59
MarwanHalabi <m.halvi@hotmail.com>
2025-08-19 10:46:37 +0300

    matrix workflows

--- tests/test_example.py
+++ tests/test_example.py
@@ -1,6 +1,8 @@
 import os
 import unittest
 from selenium import webdriver
+import allure
+from allure import severity_level
 from driver_factory import get_driver
 
 OLLAMA_URL = os.environ.get('OLLAMA_URL', 'http://54.247.216.178:3000')  # Default to localhost if not set
@@ -15,6 +17,13 @@ class ExampleTestCase(unittest.TestCase):
     def tearDown(self):
         self.driver.quit()
 
+    @allure.title("Test home endpoint")
+    @allure.description("Description............")
+    @allure.severity(severity_level.CRITICAL)
+    @allure.label("owner", "API Team")
+    @allure.link("https://dev.example.com/", name="Website")
+    @allure.issue("AUTH-123")
+    @allure.testcase("TMS-456")
     def test_page_title(self):
         self.assertIn('Ollama UI', self.driver.title)
 
